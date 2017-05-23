5c775800b6821910453a52fd0cfe67c5b82b68c4
zhangshirly <1872120168@qq.com>
2017-05-23 16:10:17 +0800

    fix deploy

--- windows_deploy.bat
+++ windows_deploy.bat
@@ -1,6 +1,9 @@
 # 无防火墙限制时可用
 rsync -rtzvlC --exclude upload --exclude public_dist --exclude node_modules --exclude '.git' ./ "root@211.159.157.171":"/data/www/club"
 
-#有防火墙限制时
-pscp -r dir ../club root@211.159.157.171:/data/www/
+#有防火墙限制时 整个项目部署
+pscp -r ../club root@211.159.157.171:/data/www/
+
+# 部分常改动文件部署
+pscp -r package.json windows_deploy.bat app.js gulpfile.js gulpfile2.js server source public test root@211.159.157.171:/data/www/
 
---
+++ windows_deploy_min.bat
@@ -0,0 +1,3 @@
+
+# 部分常改动文件部署
+pscp -r package.json windows_deploy.bat app.js gulpfile.js gulpfile2.js server source public test root@211.159.157.171:/data/www/
