61933238a20cdfdcb1f2e275e27630f67af69e9f
zhangshirly <1872120168@qq.com>
2017-05-23 16:11:53 +0800

    fix start

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-forever stop app.js
+forever stop -c ./bin/node-v6.10.3-linux-x64/bin/node app.js
 # set debug false
 sed -i '0,/debug: true/s/debug: true/debug: false/' config.js
 # npm install
